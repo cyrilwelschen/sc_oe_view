8fc3a9b15188f5c5f981acfa6e29e5402f40f35f
Cyril Welschen <cj.welschen@gmail.com>
2017-10-12 19:55:30 +0200

    falsk and js

--- logic.py
+++ logic.py
@@ -3,9 +3,50 @@
 from flask import Flask
 from flask import request
 from flask import render_template
+import re
+import pickle
+import random
 
 app = Flask(__name__)
 
-@app.route('/')
+def parents(oe):
+    arr = oe.split("-")
+    nr = len(arr)
+    pa = []
+    for i,o in enumerate(arr):
+        if len(arr[:i]) != 0:
+            if len(arr[:i]) == 1:
+                pa.append(arr[:i][0])
+            else:
+                s =""
+                for j in arr[:i]:
+                   s += j+"-"
+                pa.append(s[:-1])
+    return pa
+
+def minus(num):
+    return num-1
+
+@app.route('/', methods=['POST', 'GET'])
 def logic():
-    return "Hello from the other side"
+    if request.method == 'POST':
+        file = open("./templates/oes.txt", "r")
+        data = file.readlines()
+        file.close()
+        return render_template('main.html', data=data)
+
+    if request.method == 'GET':
+        file = open("./templates/oes.txt", "r")
+        data = file.readlines()
+        file.close()
+        data_new = []
+        pattern = re.compile("[A-Z]{3}[^ ].*")
+        for d in data:
+            if pattern.match(d):
+                data_new.append(d.split(";;")[0])
+        dumpp = []
+        for d in data_new:
+            dumpp.append({'name': d, 'show': "y", 'pos_x': 1500*random.random(), 'pos_y': 1000*random.random(), 'parents': parents(d), 'level': len(d.split("-"))})
+        pickle.dump(dumpp, open("./all_oes.txt","wb"))
+        return render_template('main.html', data=pickle.load(open("./all_oes.txt","rb")),
+                               minus=minus)
